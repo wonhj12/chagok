b331261caa8fac9f1dbcb812f7c937cab19ed462
wonhj12 <wonhj12@naver.com>
2025-02-20 19:12:37 +0900

    feat: 소셜 로그인 버튼 구현

---
+++ lib/components/login/login_button.dart
@@ -0,0 +1,48 @@
+import 'package:chagok/utils/palette.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginButton extends StatelessWidget {
+  final String icon;
+  final String text;
+  final Color background;
+  final Function()? onTap;
+  const LoginButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.background,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        height: 48,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          color: background,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/$icon',
+              width: 20,
+              height: 20,
+            ),
+            Text(
+              text,
+              style: Palette.callout.copyWith(fontWeight: FontWeight.w500),
+            ),
+            SizedBox(width: 20)
+          ],
+        ),
+      ),
+    );
+  }
+}
