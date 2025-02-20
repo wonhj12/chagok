6f4d6b40b965143775c6e8beb4de5796b95de433
wonhj12 <wonhj12@naver.com>
2025-02-20 14:21:58 +0900

    feat: 앱 실행 초기화 로직 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:chagok/utils/app_router.dart';
+import 'package:chagok/utils/custom_theme_data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MainApp());
 }
+
+// 라우터
+final _router = AppRouter.getRouter();
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: _router,
+      theme: CustomThemeData.light,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
