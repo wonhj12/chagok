327b3742291b2801605cad372838264aedcdcbd2
wonhj12 <wonhj12@naver.com>
2025-02-21 00:35:21 +0900

    design: onPrimary, onSecondary 생상을 surface 색상으로 변경

--- lib/utils/custom_theme_data.dart
+++ lib/utils/custom_theme_data.dart
@@ -11,9 +11,9 @@ class CustomThemeData {
     // 앱 전체적인 테마
     colorScheme: const ColorScheme.light(
       primary: Palette.primary,
-      onPrimary: Palette.container,
+      onPrimary: Palette.surface,
       secondary: Palette.secondary,
-      onSecondary: Palette.onSurface,
+      onSecondary: Palette.surface,
       surface: Palette.surface,
       onSurface: Palette.onSurface,
       onSurfaceVariant: Palette.onSurfaceVariant,
