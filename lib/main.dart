d0f700e064bb1327f45d64731817d0d3a6787dbf
wonhj12 <wonhj12@naver.com>
2025-02-20 19:12:12 +0900

    mod: 카카오 init 로직 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:chagok/utils/app_router.dart';
 import 'package:chagok/utils/custom_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
@@ -11,6 +12,7 @@ void main() async {
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']);
 
   runApp(const MainApp());
 }
