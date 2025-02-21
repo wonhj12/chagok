e9241dd37217ea87662d3a47b72660bb0a644065
wonhj12 <wonhj12@naver.com>
2025-02-21 13:37:19 +0900

    feat: 선택된 날짜가 포함된 주차를 계산하는 함수 구현

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -6,6 +6,8 @@ class TodoModel with ChangeNotifier {
   /// 현재 선택된 날짜
   DateTime selectedDate = DateTime.now();
 
+  List<DateTime> selectedWeek = [];
+
   /// 선택된 날짜의 todo 리스트
   List<Todo> todos = [
     Todo(
@@ -36,4 +38,20 @@ class TodoModel with ChangeNotifier {
       isCompleted: false,
     ),
   ];
+
+  TodoModel() {
+    // 모델이 처음 생성될 때 오늘이 포함된 주차 계산
+    getSelectedWeek();
+  }
+
+  /// 선택된 날짜가 포함된 주의 날짜를 반환
+  void getSelectedWeek() {
+    // 선택한 날짜가 포함된 주의 월요일을 계산
+    DateTime sunday =
+        selectedDate.subtract(Duration(days: selectedDate.weekday));
+
+    // 일요일부터 토요일까지의 날짜 리스트 생성
+    selectedWeek =
+        List.generate(7, (index) => sunday.add(Duration(days: index)));
+  }
 }
