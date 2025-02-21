f5305b9631eb1f951ac0746a29504f82aad7d362
wonhj12 <wonhj12@naver.com>
2025-02-21 11:33:02 +0900

    design: HomeView 화면 구성 - TodoModel에 임시로 Todo 더미 데이터 입력

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,4 +1,5 @@
 import 'package:chagok/models/todo.dart';
+import 'package:chagok/utils/enums/emotion.dart';
 import 'package:flutter/material.dart';
 
 class TodoModel with ChangeNotifier {
@@ -6,5 +7,33 @@ class TodoModel with ChangeNotifier {
   DateTime selectedDate = DateTime.now();
 
   /// 선택된 날짜의 todo 리스트
-  List<Todo> todo = [];
+  List<Todo> todos = [
+    Todo(
+      title: '테스트 1',
+      emotion: Emotion.happy,
+      time: TimeOfDay(hour: 9, minute: 0),
+      isCompleted: true,
+    ),
+    Todo(
+      title: '테스트 2',
+      emotion: Emotion.longing,
+      isCompleted: false,
+    ),
+    Todo(
+      title: '테스트 3',
+      emotion: Emotion.soso,
+      isCompleted: false,
+    ),
+    Todo(
+      title: '테스트 4',
+      emotion: Emotion.sad,
+      time: TimeOfDay(hour: 13, minute: 30),
+      isCompleted: false,
+    ),
+    Todo(
+      title: '테스트 5',
+      emotion: Emotion.hate,
+      isCompleted: false,
+    ),
+  ];
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:chagok/components/common/custom_scaffold.dart';
+import 'package:chagok/components/home/date_tile.dart';
+import 'package:chagok/components/home/todo_list_tile.dart';
 import 'package:chagok/utils/palette.dart';
 import 'package:chagok/view_models/home_view_model.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +16,70 @@ class HomeView extends StatelessWidget {
     return CustomScaffold(
       body: Column(
         children: [
-          Text('금요일', style: Palette.largeTitleSemibold),
+          // 날짜 기본 정보
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // 요일
+              Text('금요일', style: Palette.largeTitleSemibold),
+
+              // 날짜
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text('2월 21일', style: Palette.headline),
+                  Text(
+                    '2025',
+                    style: Palette.body.copyWith(
+                      color: Palette.onSurfaceVariant,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 32),
+
+          // 날짜 선택
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              DateTile(day: 16, weekDay: '일', isSelected: false),
+              DateTile(day: 17, weekDay: '월', isSelected: false),
+              DateTile(day: 18, weekDay: '화', isSelected: false),
+              DateTile(day: 19, weekDay: '수', isSelected: false),
+              DateTile(day: 20, weekDay: '목', isSelected: false),
+              DateTile(day: 21, weekDay: '금', isSelected: true),
+              DateTile(day: 22, weekDay: '토', isSelected: false),
+            ],
+          ),
+          const SizedBox(height: 24),
+
+          // 할 일 목록
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Palette.container,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemBuilder: (_, index) => TodoListTile(
+                  todo: homeViewModel.todoModel.todos[index],
+                ),
+                separatorBuilder: (_, __) => Divider(
+                  height: 0,
+                  thickness: 0.5,
+                  color: Palette.onSurfaceVariant,
+                ),
+                itemCount: homeViewModel.todoModel.todos.length,
+              ),
+            ),
+          )
         ],
       ),
       floatingActionButton: IconButton.filled(
