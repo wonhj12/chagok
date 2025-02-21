8195d5b6517daafb91efccd7f964bf4a3f8d8899
wonhj12 <wonhj12@naver.com>
2025-02-21 19:42:24 +0900

    mod: 감정 설명 추가

--- lib/utils/enums/emotion.dart
+++ lib/utils/enums/emotion.dart
@@ -1,9 +1,14 @@
 enum Emotion {
-  happy,
-  longing,
-  soso,
-  hate,
-  sad;
+  happy(description: '행복해요'),
+  longing(description: '기대돼요'),
+  soso(description: '그저 그래요'),
+  hate(description: '싫어요'),
+  sad(description: '슬퍼요');
+
+  /// 감정 설명
+  final String description;
+
+  const Emotion({required this.description});
 
   factory Emotion.fromString(String name) {
     return Emotion.values.firstWhere((value) => value.name == name);
