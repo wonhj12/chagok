0d08c36db0b720cded8f51057794cb7ec1cf5eeb
wonhj12 <wonhj12@naver.com>
2025-02-21 11:30:58 +0900

    feat: todo 리스트에 표시될 TodoListTile 구현

---
+++ lib/components/home/todo_list_tile.dart
@@ -0,0 +1,48 @@
+import 'package:chagok/components/home/todo_tag_box.dart';
+import 'package:chagok/models/todo.dart';
+import 'package:chagok/utils/enums/todo_tag.dart';
+import 'package:chagok/utils/palette.dart';
+import 'package:flutter/material.dart';
+
+class TodoListTile extends StatelessWidget {
+  final Todo todo;
+  const TodoListTile({super.key, required this.todo});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 48,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // 감정 아이콘
+          Image.asset(
+            width: 20,
+            height: 20,
+            'assets/icons/emotion_${todo.emotion.name}.png',
+          ),
+          const SizedBox(width: 20),
+
+          // 제목
+          Text(todo.title, style: Palette.body),
+          const SizedBox(width: 12),
+
+          // 완료 표시
+          if (todo.isCompleted) TodoTagBox(tag: TodoTag.completed),
+          const Spacer(),
+
+          // 시간
+          Text(
+            todo.time != null
+                ? '${todo.time!.hour.toString().padLeft(2, '0')}:${todo.time!.minute.toString().padLeft(2, '0')}'
+                : '',
+            style: Palette.caption.copyWith(
+              color: Palette.onSurfaceVariant,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
