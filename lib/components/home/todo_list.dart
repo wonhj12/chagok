98a46e9a085450029c4b4b215dff95e22dcc3141
wonhj12 <wonhj12@naver.com>
2025-02-23 13:04:59 +0900

    feat: 일정 없음 표시 구현

--- lib/components/home/todo_list.dart
+++ lib/components/home/todo_list.dart
@@ -19,23 +19,46 @@ class TodoList extends StatelessWidget {
           color: Palette.container,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: ListView.separated(
-          shrinkWrap: true,
-          physics: const ClampingScrollPhysics(),
-          itemBuilder: (_, index) => TodoListTile(
-            todo: todos[index],
-            onTapTodo: () => onTapTodo(todos[index]),
-          ),
-          separatorBuilder: (_, __) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: DottedLine(
-              dashColor: Palette.onSurfaceVariant,
-              dashLength: 2,
-              dashGapLength: 5,
-            ),
-          ),
-          itemCount: todos.length,
-        ),
+        child: todos.isNotEmpty
+            ? ListView.separated(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemBuilder: (_, index) => TodoListTile(
+                  todo: todos[index],
+                  onTapTodo: () => onTapTodo(todos[index]),
+                ),
+                separatorBuilder: (_, __) => Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: DottedLine(
+                    dashColor: Palette.onSurfaceVariant,
+                    dashLength: 2,
+                    dashGapLength: 5,
+                  ),
+                ),
+                itemCount: todos.length,
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // 이미지
+                    Image.asset(
+                      'assets/icons/sunglass.png',
+                      width: 128,
+                      height: 128,
+                    ),
+                    const SizedBox(height: 24),
+
+                    // 일정 없음 문구
+                    Text(
+                      '일정이 없네요!\n새 일정을 추가해 보세요.',
+                      style: Palette.body,
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
