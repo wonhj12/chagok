6d4933196e7d23f779e2b8931b6fed86f09c6489
wonhj12 <wonhj12@naver.com>
2025-02-26 22:36:48 +0900

    mod: 스와이프 오른쪽만 되도록 수정 - 완료 태그 표시 애니메이션 추가

--- lib/components/home/todo_list_tile.dart
+++ lib/components/home/todo_list_tile.dart
@@ -23,70 +23,73 @@ class TodoListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Slidable(
       key: ValueKey(todo),
-      startActionPane: ActionPane(
-        motion: const BehindMotion(),
-        extentRatio: 0.25,
-        children: [
-          SlidableAction(
-            onPressed: (_) => onPressedComplete(),
-            backgroundColor: todo.isCompleted ? Palette.error : Palette.success,
-            foregroundColor: Palette.surface,
-            label: todo.isCompleted ? '미완료' : '완료',
-          )
-        ],
-      ),
       endActionPane: ActionPane(
         dismissible: DismissiblePane(
           onDismissed: onDismissed,
           closeOnCancel: true,
         ),
-        motion: const BehindMotion(),
-        extentRatio: 0.25,
+        motion: const DrawerMotion(),
+        extentRatio: 0.4,
         children: [
+          SlidableAction(
+            onPressed: (_) => onPressedComplete(),
+            backgroundColor: Palette.success,
+            foregroundColor: Palette.surface,
+            icon: todo.isCompleted
+                ? Icons.cancel_outlined
+                : Icons.check_circle_outline_rounded,
+          ),
           SlidableAction(
             onPressed: (_) => onDismissed(),
             backgroundColor: Palette.error,
             foregroundColor: Palette.surface,
-            label: '삭제',
+            icon: Icons.delete_rounded,
           ),
         ],
       ),
       child: Material(
         color: Colors.transparent,
-        child: InkWell(
-          onTap: onTapTodo,
-          child: Container(
-            width: double.infinity,
-            height: 48,
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            color: Palette.container,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // 감정 아이콘
-                Image.asset(
-                  width: 20,
-                  height: 20,
-                  'assets/icons/emotion_${todo.emotion.name}.png',
-                ),
-                const SizedBox(width: 20),
-
-                // 제목
-                Text(todo.title, style: Palette.body),
-                const SizedBox(width: 12),
-
-                // 완료 표시
-                if (todo.isCompleted) TodoTagBox(tag: TodoTag.completed),
-                const Spacer(),
-
-                // 시간
-                Text(
-                  todo.time != null ? timeOfDayToString(todo.time!) : '',
-                  style: Palette.caption.copyWith(
-                    color: Palette.onSurfaceVariant,
+        child: Ink(
+          color: Palette.container,
+          child: InkWell(
+            onTap: onTapTodo,
+            child: Container(
+              width: double.infinity,
+              height: 48,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // 감정 아이콘
+                  Image.asset(
+                    width: 20,
+                    height: 20,
+                    'assets/icons/emotion_${todo.emotion.name}.png',
                   ),
-                )
-              ],
+                  const SizedBox(width: 20),
+
+                  // 제목
+                  Text(todo.title, style: Palette.body),
+                  const SizedBox(width: 12),
+
+                  // 완료 표시
+                  AnimatedScale(
+                    scale: todo.isCompleted ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 300), // 애니메이션 시간 조정
+                    curve: Curves.easeInOut, // 애니메이션 곡선
+                    child: TodoTagBox(tag: TodoTag.completed),
+                  ),
+                  const Spacer(),
+
+                  // 시간
+                  Text(
+                    todo.time != null ? timeOfDayToString(todo.time!) : '',
+                    style: Palette.caption.copyWith(
+                      color: Palette.onSurfaceVariant,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
